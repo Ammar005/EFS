b57453b2989cdaee65aa623e6f908c5b6a515e76
Ammar <aabid@unomaha.edu>
2016-09-15 15:30:43 -0500

    added where cluse to my Customer show()  function

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -27,7 +27,8 @@ class CustomerController extends Controller
         $investments= Investment::where('customer_id',$id)->get();
 
         $StockTotal= Stock::select(DB::raw('sum(purchase_price) as price_total'))-> where('customer_id',$id)->first();
-        $InvestmentTotal= Investment::select(DB::raw('sum(public.investments.acquired_value) as total_investment'))-> where('customer_id',$id)->first();
+        $InvestmentTotal=0;
+        // Investment::select(DB::raw('sum(acquired_value) as total_investment'))->where('customer_id',$id)->first();
 
         return view('customers.show',compact('customer', 'stocks','investments','StockTotal', 'InvestmentTotal'));
     }
